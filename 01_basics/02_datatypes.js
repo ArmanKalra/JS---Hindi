e6ae3b84eeee1d01b14935c29cd6c509de32d05e
ArmanKalra <156878964+ArmanKalra@users.noreply.github.com>
2025-04-08 09:06:03 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" // treating js code as newer version of javascript.
+
+// alert(3+3) // we are using nodejs not browser
+
+/* Types of variables
+
+number => 2 to power 53
+bigint 
+string=> ""
+boolean => true/false
+null => standalone value
+undefined => value is not assigned yet
+symbol => unique
+
+object 
+*/
+
+let age = 12
+let name = "arman"
+let flag = true
+let temp = null
+let plan
+
+console.log(typeof age);
+console.log(typeof name)
+console.log(typeof flag)
+console.log(typeof temp) // ouptup for null will be an object
+console.log(typeof plan) // output for undefined will be undefined
+
