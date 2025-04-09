f5a6302157b43b6e3b39f00a3122b9eaeeaa3849
ArmanKalra <156878964+ArmanKalra@users.noreply.github.com>
2025-04-09 19:06:39 +0000

    datatypes_summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,30 @@
+// Primitive 
+
+// 7 types : Integer , string , null , undefined , symbol, boolean , Bigint
+
+// Symbol
+// const id = Symbol('456')
+// const anotherId = Symbol('456')
+// console.log(id===anotherId);    // gives unique values 
+// console.log(id)
+// console.log(anotherId)
+
+// const bigNumber =4546465656565656555n
+// console.log(typeof bigNumber);
+
+
+// Non primitive 
+ // arrays, objects , functions 
+
+ const heros = ["shaktiman","nagarjun","buri buri"]
+ let a_obj={
+    name : "arman",
+    age:20
+ }
+ const new_func= function(){
+    console.log("Hello")
+ } 
+ console.log(typeof heros);
+ console.log(typeof a_obj);
+ console.log(typeof new_func);
+ 
