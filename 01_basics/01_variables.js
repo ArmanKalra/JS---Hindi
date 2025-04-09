e182c2d351f08aaba8812870e9ae727d6d5f1f49
ArmanKalra <156878964+ArmanKalra@users.noreply.github.com>
2025-04-09 11:39:31 +0000

    Conversion of datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId=45642
+let accountEmail="arman5@gmail.com"
+var accountPass="njckdcj"
+accountCity="Muktsar"
+
+// accountId=47878   // not allowed to change constant 
+
+accountEmail="new@gmail.com"
+accountPass="kjcjdhv"
+accountCity="Rajpura"
+let accountState
+
+console.log(accountId)
+
+/*
+     var should never be used 
+     beacuse var has global scope instead of functional scope.
+*/
+
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
